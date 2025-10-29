b1ce25aedec0b98d90df70a565e148da486f2a4e
KelvinGuima <vcmkelvin@outlook.com>
2025-10-28 22:49:59 -0300

    chore: restructure skeleton (base code without full logic)

---
+++ backend/src/config/db.js
@@ -0,0 +1,10 @@
+const { Sequelize } = require('sequelize');
+require('./dotenv');
+
+const sequelize = new Sequelize(process.env.DB_NAME || 'supporthub', process.env.DB_USER || 'root', process.env.DB_PASS || '', {
+  host: process.env.DB_HOST || 'localhost',
+  dialect: 'mysql',
+  logging: false
+});
+
+module.exports = { sequelize };
